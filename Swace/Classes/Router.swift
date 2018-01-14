17cde28dd508f645db610c23cb2e44926286d811
Andreas <a.oshtman@gmail.com>
2018-01-14 18:01:16 +0100

    - Allow routing schemes to be either 'strict' or non 'strict'. This to allow routing inside app (to settings, photos etc) to endpoints that do not really follow a real url convention with schemes of type ://

--- Swace/Classes/Router.swift
+++ Swace/Classes/Router.swift
@@ -40,11 +40,13 @@ public extension Dictionary {
 
 public class Scheme: Hashable {
     public let name: String
+    public let strict: Bool
 
-    public init(name: String) {
+    public init(name: String, strict: Bool = true) {
         self.name = name
+        self.strict = strict
     }
-
+    public var full: String { return strict ? name + "://" : name }
     public var hashValue: Int { return name.hashValue }
     public static func ==(lhs: Scheme, rhs: Scheme) -> Bool { return lhs.name == rhs.name }
 }
@@ -61,42 +63,44 @@ public class Router {
     
     // Navigating from outside app
     public func resolve(_ url: URL, options: [UIApplicationOpenURLOptionsKey: Any]) throws {
-        guard let scheme = url.scheme else { throw RoutingError.urlSchemeMissing }
-        try Router.route(for: url)
+        guard let schemeName = url.scheme else { throw RoutingError.urlSchemeMissing }
+        let scheme = Scheme(name: schemeName)
+
+        try Router.route(for: url, using: scheme)
         
         var parsedOptions = [String: Any]()
         options.forEach { (k,v) in parsedOptions[k.rawValue] = v }
 
-        let routeScheme = scheme + "://"
-        let path = url.absoluteString.replacingOccurrences(of: routeScheme , with: "")
+        let path = url.absoluteString.replacingOccurrences(of: scheme.full , with: "")
         try Router.navigate(to: path, scheme: scheme, options: parsedOptions)
     }
     
     // Navigating from in-app
     public class func navigate(to module: RoutableModule, options: [String: Any] = [:], from: Route? = nil) throws {
         guard let route = module.route else { throw RoutingError.missingModuleRoute }
-        try navigate(to: module.path, scheme: route.scheme.name, from: from, options: options)
+        try navigate(to: module.path, scheme: route.scheme, from: from, options: options)
     }
     
     public class func navigate(to urn: String) throws {
         try navigate(to: urn)
     }
     
-    private class func navigate(to urn: String, scheme: String, from : Route? = nil, options: [String: Any]? = nil) throws {
-        guard let url = URL(string: scheme + "://" + urn) else { throw RoutingError.invalidURL }
+    private class func navigate(to urn: String, scheme: Scheme, from : Route? = nil, options: [String: Any]? = nil) throws {
+        guard let url = URL(string: scheme.full + urn) else { throw RoutingError.invalidURL }
         
         var queryParams = parseURL(url)
         queryParams.merge(options ?? [:]) { (k1, k2) -> Any in return k1 }
         
-        let matchingRoute = try route(for: url)
+        let matchingRoute = try route(for: url, using: scheme)
         try matchingRoute.take(url: url, arguments: queryParams, from: from)
     }
     
-    @discardableResult fileprivate class func route(for url: URL) throws -> Route {
+    @discardableResult fileprivate class func route(for url: URL, using scheme: Scheme) throws -> Route {
         guard let schemeName = url.scheme else { throw RoutingError.urlSchemeMissing }
-        guard let match = Router.internalRoutes[Scheme(name: schemeName)]?.filter({
-            print("Matching \(url.host) against \($0.path)")
-            return $0.path == url.host
+        guard let match = Router.internalRoutes[scheme]?.filter({
+            let path = url.absoluteString[scheme.full.endIndex..<url.absoluteString.endIndex]
+            print("Matching \(path) against \($0.path)")
+            return $0.path == path
         }).first else { throw RoutingError.doesNotExist }
         return match
     }
